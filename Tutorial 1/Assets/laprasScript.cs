1fb31f18e74faebc23115454b5694499961de3a6
Sninja1234 <meganin.j@gmail.com>
2017-03-25 17:21:33 -0400

    Tutorial Folder
    Tutorial Folder

---
+++ Tutorial 1/Assets/laprasScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class laprasScript : MonoBehaviour {
+	public Button attackButton;
+	public GameObject hose;
+
+
+	// Use this for initialization
+	void Start () {
+		attackButton.onClick.AddListener (attackButtonDown);
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (false);
+
+	}
+	IEnumerator Wait(){
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (true);
+		yield return new WaitForSeconds (2);
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (false);
+	}
+	void attackButtonDown(){
+		StartCoroutine (Wait ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
